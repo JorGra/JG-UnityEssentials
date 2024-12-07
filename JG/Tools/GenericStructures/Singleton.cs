200ae3f66b3a0acd1ae68c8ee4155341ee3985f7
Joris Grau <joris.grau2@gmail.com>
2024-12-07 20:23:34 +0100

    Updated Singleton class

--- JG/Tools/GenericStructures/Singleton.cs
+++ JG/Tools/GenericStructures/Singleton.cs
@@ -1,5 +1,4 @@
 using UnityEngine;
-
 namespace JG.Tools
 {
     public abstract class Singleton<T> : MonoBehaviour where T : MonoBehaviour
@@ -7,13 +6,20 @@ namespace JG.Tools
         private static T _instance;
         private static readonly object _lock = new object();
         private static bool _applicationIsQuitting = false;
-
         [SerializeField] private bool _persistent = false;
 
         public static T Instance
         {
             get
             {
+                // Reset flag when accessing instance in editor
+#if UNITY_EDITOR
+                if (!Application.isPlaying && _applicationIsQuitting)
+                {
+                    _applicationIsQuitting = false;
+                }
+#endif
+
                 if (_applicationIsQuitting)
                 {
                     Debug.LogWarning($"[Singleton] Instance '{typeof(T)}' already destroyed on application quit. Won't create again - returning null.");
@@ -24,14 +30,12 @@ namespace JG.Tools
                 {
                     if (_instance == null)
                     {
-                        _instance = (T)FindObjectOfType(typeof(T));
-
+                        _instance = (T)FindObjectOfType(typeof(T), true);
                         if (_instance == null)
                         {
                             GameObject singleton = new GameObject();
                             _instance = singleton.AddComponent<T>();
                             singleton.name = $"[Singleton] {typeof(T).Name}";
-
                             Debug.Log($"[Singleton] An instance of {typeof(T)} is needed in the scene, so '{singleton.name}' was created.");
                         }
                         else
@@ -39,7 +43,6 @@ namespace JG.Tools
                             Debug.Log($"[Singleton] Using instance already created: {_instance.gameObject.name}");
                         }
                     }
-
                     return _instance;
                 }
             }
@@ -55,8 +58,16 @@ namespace JG.Tools
             }
             else if (_instance != this)
             {
-                Debug.LogWarning($"[Singleton] Another instance of {typeof(T)} already exists. Destroying this duplicate.");
-                Destroy(gameObject);
+                // Check if the existing instance has been destroyed
+                if (_instance == null)
+                {
+                    _instance = this as T;
+                }
+                else
+                {
+                    Debug.LogWarning($"[Singleton] Another instance of {typeof(T)} already exists. Destroying this duplicate.");
+                    Destroy(gameObject);
+                }
             }
         }
 
@@ -67,9 +78,11 @@ namespace JG.Tools
 
         protected virtual void OnDestroy()
         {
+            // Only nullify if this is the current instance
             if (_instance == this)
             {
                 _instance = null;
+                _applicationIsQuitting = false;
             }
         }
 
