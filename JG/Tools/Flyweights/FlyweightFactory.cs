88e0957d3ccaedc984ce9c53d1d16fcf42ec1d47
Joris Grau <joris.grau2@gmail.com>
2024-12-25 19:48:51 +0100

    Added Flyweight System

---
+++ JG/Tools/Flyweights/FlyweightFactory.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+using JG.Tools;
+using UnityEngine;
+using UnityEngine.Pool;
+
+
+namespace JG.Flyweights
+{
+    public class FlyweightFactory : PersistentSingleton<FlyweightFactory>
+    {
+        [SerializeField] bool collectionCheck = true;
+        [SerializeField] int defaultCapacity = 10;
+        [SerializeField] int maxPoolSize = 100;
+
+        readonly Dictionary<string, IObjectPool<Flyweight>> pools = new();
+
+
+        protected override void Awake()
+        {
+            base.Awake();
+        }
+
+        public static Flyweight Spawn(FlyweightSettings settings) => instance.GetPoolFor(settings)?.Get();
+
+        public static Flyweight Spawn(FlyweightSettings settings, Vector3 position, Quaternion rotation)
+        {
+            var f = instance.GetPoolFor(settings)?.Get();
+            f.transform.SetPositionAndRotation(position, rotation);
+            return f;
+        }
+
+        public static Flyweight Spawn(FlyweightSettings settings, Vector3 position, Quaternion rotation, Transform parent)
+        {
+            var f = instance.GetPoolFor(settings)?.Get();
+            f.transform.SetPositionAndRotation(position, rotation);
+            f.transform.SetParent(parent);
+            return f;
+        }
+        public static void ReturnToPool(Flyweight f) => instance.GetPoolFor(f.settings)?.Release(f);
+
+        IObjectPool<Flyweight> GetPoolFor(FlyweightSettings settings)
+        {
+            if(settings == null)
+            {
+                Debug.LogError("FlyweightSettings is null");
+                return null;
+            }
+
+
+            if (pools.TryGetValue(settings.Name, out IObjectPool<Flyweight> pool)) return pool;
+
+            pool = new ObjectPool<Flyweight>(
+                settings.Create,
+                settings.OnGet,
+                settings.OnRelease,
+                settings.OnDestroyPoolObject,
+                collectionCheck,
+                defaultCapacity,
+                maxPoolSize
+            );
+            pools.Add(settings.Name, pool);
+            return pool;
+        }
+    }
+}
