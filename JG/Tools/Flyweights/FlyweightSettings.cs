74bb6e06a3f58cac027b9a30f8a861af9abd6d64
Joris Grau <joris.grau2@gmail.com>
2025-06-30 00:09:04 +0200

    Update FlyweightSettings.cs

--- JG/Tools/Flyweights/FlyweightSettings.cs
+++ JG/Tools/Flyweights/FlyweightSettings.cs
@@ -1,15 +1,21 @@
 ﻿using UnityEngine;
 
 
-namespace JG.Flyweights { 
-public abstract class FlyweightSettings : ScriptableObject
+namespace JG.Flyweights
 {
-    public GameObject prefab;
-    public string Name => prefab.name;
+    public abstract class FlyweightSettings : ScriptableObject
+    {
+        public GameObject prefab;
+        public string Name => prefab.name;
 
-    public abstract Flyweight Create();
+        public abstract Flyweight Create();
 
-    public virtual void OnGet(Flyweight f) => f.gameObject.SetActive(true);
-    public virtual void OnRelease(Flyweight f) => f.gameObject.SetActive(false);
-    public virtual void OnDestroyPoolObject(Flyweight f) => Destroy(f.gameObject);
-}}
+        public virtual void OnGet(Flyweight f) => f.gameObject.SetActive(true);
+        public virtual void OnRelease(Flyweight f) => f.gameObject.SetActive(false);
+        public virtual void OnDestroyPoolObject(Flyweight f)
+        {
+            if (f == null) return;
+            Destroy(f.gameObject);
+        }
+    }
+}
