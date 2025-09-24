60d985a9665f5425fea2ab42d2c003a57b102e63
Joris Grau <joris.grau2@gmail.com>
2025-09-24 21:52:22 +0200

    Update EventBus.cs
    fixes issue when eventbus updates while it is removing entries

--- JG/Tools/EventBus/EventBus.cs
+++ JG/Tools/EventBus/EventBus.cs
@@ -6,21 +6,42 @@ public interface IEvent { }
 public static class EventBus<T> where T : IEvent
 {
     static readonly HashSet<IEventBinding<T>> bindings = new HashSet<IEventBinding<T>>();
+    static readonly object bindingsLock = new object();
 
     public static void Register(IEventBinding<T> binding)
     {
-        bindings.Add(binding);
+        lock (bindingsLock)
+        {
+            bindings.Add(binding);
+        }
     }
 
     public static void Deregister(IEventBinding<T> binding)
     {
-        bindings.Remove(binding);
+        lock (bindingsLock)
+        {
+            bindings.Remove(binding);
+        }
     }
 
     public static void Raise(T @event)
     {
-        foreach (var binding in bindings)
+        IEventBinding<T>[] snapshot;
+
+        lock (bindingsLock)
         {
+            if (bindings.Count == 0)
+            {
+                return;
+            }
+
+            snapshot = new IEventBinding<T>[bindings.Count];
+            bindings.CopyTo(snapshot);
+        }
+
+        for (int i = 0; i < snapshot.Length; i++)
+        {
+            var binding = snapshot[i];
             binding.OnEvent.Invoke(@event);
             binding.OnEventNoArgs.Invoke();
         }
